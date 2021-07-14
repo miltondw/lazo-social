2036e3ca741b04ef9a994851cadc56f9949cca27
miltondw <estradamilton2001@gmail.com>
2021-07-14 16:24:57 -0500

    Change env

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  env: {
-    BASE_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://miltondw:6N3ZMnJs46qTAG2t@cluster0.s2fjm.mongodb.net/next_ecommerce?retryWrites=true&w=majority",
-    ACCESS_TOKEN_SECRET: "`S:&xbffA5LFD<!e5SptY^#w!,B!ajz`>^?%u',z`D\4zx%t",
-    REFRESH_TOKEN_SECRET:
-      "'RaR!;V:BCYme>Z+p<k_2ntu8mM,J^'!!8K!$>TE-QwZ?pPw`QfykhwUeMSQ6@MC+2kZMdT:T",
-    PAYPAL_CLIENT_ID:
-      "ATOac4-IOBLXpIpLLunmKe8I4mO5TK9WaouenC1u10xQfbRWn48AL9J7smShxCg-EXxdywaXbL0bkhfK",
-    CLOUD_UPDATE_PRESET: "nextjs_store",
-    CLOUD_NAME: "daekebmip",
-    CLOUD_API: "https://api.cloudinary.com/v1_1/daekebmip/image/upload",
-  },
   images: {
     domains: [
       "res.cloudinary.com",
